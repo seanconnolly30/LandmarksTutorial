b24ad1cd0e85a0aada926dba1292cd4d71d07f53
Sean Connolly <seapconnolly@gmail.com>
2024-05-29 20:14:45 -0400

    Initial Commit

---
+++ LandmarksTutorial/LandmarksTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTutorialApp.swift
+//  LandmarksTutorial
+//
+//  Created by Sean Connolly on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
